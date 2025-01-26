a2db3a637c470b0cd471626937b3a7eac6a0bc11
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2025-01-26 20:46:15 +0100

    Added EndDate and TrialDuration calculation

--- Application/ClinicalTrials/Commands/ClinicalTrialCreate.cs
+++ Application/ClinicalTrials/Commands/ClinicalTrialCreate.cs
@@ -20,9 +20,32 @@ namespace Application.ClinicalTrials.Commands
             }
             public async Task Handle(Command request, CancellationToken cancellationToken)
             {
-                _genericRepository.AddRange(request.ClinicalTrials);
+                var clinicalTrials = SetEndDateAndTrialDuration(request.ClinicalTrials);
+                _genericRepository.AddRange(clinicalTrials);
 
                 await _genericRepository.SaveChangesAsync();
+            }
+
+            protected List<ClinicalTrial> SetEndDateAndTrialDuration(List<ClinicalTrial> clinicalTrials)
+            {
+                var result = new List<ClinicalTrial>();
+
+                foreach (var clinicalTrial in clinicalTrials)
+                {
+                    if (clinicalTrial.EndDate is null && clinicalTrial.ClinicalTrialStatusId == 2)
+                    {
+                        var endDate = DateOnly.FromDateTime(clinicalTrial.StartDate.ToDateTime(TimeOnly.Parse("00:00 AM")).AddMonths(1));
+                        clinicalTrial.EndDate = endDate;
+                        clinicalTrial.TrialDurationInDays = clinicalTrial.EndDate.Value.DayNumber - clinicalTrial.StartDate.DayNumber;
+                        result.Add(clinicalTrial); 
+                    }
+                    else
+                    {
+                        result.Add(clinicalTrial);
+                    }                
+                }
+
+                return result;
             }
         }
     }
